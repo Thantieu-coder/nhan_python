5c389b6589c4ba1e00ee80ba2f98710355027175
Thantieu-coder <nguyendongducbdg14@gmail.com>
2025-11-26 19:38:50 +0700

    commit

---
+++ list.py/bai10.py
@@ -0,0 +1,9 @@
+# **Bài 10: Tính Tổng các số trong Danh sách**
+
+# - **Mô tả bài toán:** Cho `numbers = [5, 10, 15, 20]`. Tính và in tổng các số.
+# - **Input:** `numbers`
+# - **Output:** Tổng các số.
+# - **Ví dụ kiểm thử:** `50`
+# - **Gợi ý:** Sử dụng hàm `sum()`.
+numbers = [5, 10, 15, 20]
+print(sum(numbers))
---
+++ list.py/bai11.py
@@ -0,0 +1,9 @@
+# **Bài 11: Tìm số Lớn nhất trong Danh sách**
+
+# - **Mô tả bài toán:** Cho `points = [100, 75, 120, 90]`. Tìm và in số lớn nhất.
+# - **Input:** `points`
+# - **Output:** Số lớn nhất.
+# - **Ví dụ kiểm thử:** `120`
+# - **Gợi ý:** Sử dụng hàm `max()`.
+points = [100, 75, 120, 90]
+print(max(points))
