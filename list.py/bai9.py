583dda4cf3b14683ab8c8e48c3b7326d30263a0f
Thantieu-coder <nguyendongducbdg14@gmail.com>
2025-11-26 19:35:22 +0700

    commit

---
+++ list.py/bai9.py
@@ -0,0 +1,15 @@
+# **Bài 9: In từng Phần tử của Danh sách**
+
+# - **Mô tả bài toán:** Cho `cities = ["Hanoi", "Ho Chi Minh", "Da Nang"]`. Dùng vòng lặp `for` in từng thành phố trên một dòng.
+# - **Input:** `cities`
+# - **Output:** Các thành phố (mỗi dòng một thành phố).
+# - **Ví dụ kiểm thử:**
+    
+#     `Hanoi
+#     Ho Chi Minh
+#     Da Nang`
+    
+# - **Gợi ý:** Vòng lặp `for item in list:`.
+cities = ["Hanoi", "Ho Chi Minh", "Da Nang"]
+for cities in cities:
+    print(cities)
