617d2b2f516b38d82debb96d58f1a0fe7858bbf9
Thantieu-coder <nguyendongducbdg14@gmail.com>
2025-11-26 19:12:23 +0700

    commit

--- list.py/bai4.py
+++ list.py/bai4.py
@@ -5,3 +5,6 @@
 # - **Output:** Danh sách đã thêm.
 # - **Ví dụ kiểm thử:** `['apple', 'banana', 'orange']`
 # - **Gợi ý:** Tìm phương thức để thêm vào cuối.
+fruits=["apple","banana"]
+fruits.append("orange")
+print(fruits)
---
+++ list.py/bai5.py
@@ -0,0 +1,7 @@
+# **Bài 5: Xóa một Phần tử cụ thể (theo giá trị)**
+
+# - **Mô tả bài toán:** Cho `animals = ["cat", "dog", "fish"]`. Xóa `"dog"`. In danh sách mới.
+# - **Input:** `animals`
+# - **Output:** Danh sách đã xóa.
+# - **Ví dụ kiểm thử:** `['cat', 'fish']`
+# - **Gợi ý:** Có một phương thức để xóa phần tử theo giá trị.
