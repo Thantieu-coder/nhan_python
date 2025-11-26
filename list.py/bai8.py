976b964c98a9abc8223801e42eab13109c66bf73
Thantieu-coder <nguyendongducbdg14@gmail.com>
2025-11-26 19:32:11 +0700

    commit

---
+++ list.py/bai8.py
@@ -0,0 +1,10 @@
+# **Bài 8: Kiểm tra Phần tử có Tồn tại không**
+
+# - **Mô tả bài toán:** Cho `fruits = ["apple", "banana", "cherry"]`. Kiểm tra `"banana"` có tồn tại không. In `True` hoặc `False`.
+# - **Input:** `fruits`
+# - **Output:** `True` hoặc `False`.
+# - **Ví dụ kiểm thử:** `True`
+# - **Gợi ý:** Sử dụng toán tử `in`.
+fruits = ["apple", "banana", "cherry"]
+print("apple" in fruits)
+print("purple" in fruits)
