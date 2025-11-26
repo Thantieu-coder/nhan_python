33817b8acffd02704ad58adba2d4723e4531e441
Thantieu-coder <nguyendongducbdg14@gmail.com>
2025-11-26 19:41:57 +0700

    commit

---
+++ list.py/bai13.py
@@ -0,0 +1,10 @@
+# **Bài 13: Sắp xếp Danh sách Tăng dần**
+
+# - **Mô tả bài toán:** Cho `data = [5, 2, 8, 1]`. Sắp xếp tăng dần. In danh sách đã sắp xếp.
+# - **Input:** `data`
+# - **Output:** Danh sách đã sắp xếp.
+# - **Ví dụ kiểm thử:** `[1, 2, 5, 8]`
+# - **Gợi ý:** Sử dụng phương thức `.sort()`.
+data = [5, 2, 8, 1]
+data.sort()
+print(data)
