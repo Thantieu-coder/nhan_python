d7ffee3c19d916428f98985f99be92705dcd2ba7
Thantieu-coder <nguyendongducbdg14@gmail.com>
2025-11-26 19:29:02 +0700

    commit

---
+++ list.py/bai7.py
@@ -0,0 +1,9 @@
+# **Bài 7: Kiểm tra Độ dài Danh sách**
+
+# - **Mô tả bài toán:** Cho `items = ["laptop", "mouse", "keyboard", "monitor"]`. In số lượng phần tử.
+# - **Input:** `items`
+# - **Output:** Số lượng phần tử.
+# - **Ví dụ kiểm thử:** `4`
+# - **Gợi ý:** Sử dụng hàm `len()`.
+items = ["laptop", "mouse", "keyboard", "monitor"]
+print(len(items))
