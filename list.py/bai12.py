ab9520ab24b16c48b6784475ec8fa65ee96000a4
Thantieu-coder <nguyendongducbdg14@gmail.com>
2025-11-26 19:39:53 +0700

    commit

---
+++ list.py/bai12.py
@@ -0,0 +1,9 @@
+# **Bài 12: Tìm số Nhỏ nhất trong Danh sách**
+
+# - **Mô tả bài toán:** Cho `temperatures = [25, 18, 30, 22]`. Tìm và in số nhỏ nhất.
+# - **Input:** `temperatures`
+# - **Output:** Số nhỏ nhất.
+# - **Ví dụ kiểm thử:** `18`
+# - **Gợi ý:** Sử dụng hàm `min()`.
+temperatures = [25, 18, 30, 22]
+print(min(temperatures))
